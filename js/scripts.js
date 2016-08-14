63a32dc0484e90de071a0f545cbb17151f351b17
George <loaizageorge@gmail.com>
2016-08-14 19:23:35 -0400

    Get timer to pause and unpause on click.

--- js/scripts.js
+++ js/scripts.js
@@ -1,68 +1,112 @@
-$(document).ready(function(){
-    var minutes=25;
-    var breakMinutes=5;
-    var isWorktime = true;
-    var started = false;
-    $("#timer").html(minutes+":"+"00");
+$(document).ready(function () {
+    var minutes = 25;
+    var breakMinutes = 5;
     
-$("#start").click(function(){
-    
-    newTimer(minutes,breakMinutes,isWorktime);
-   
-
-});  
-});
-
-function newTimer(minutes,breakMinutes,isWorktime){
-    if(isWorktime){
-    var countMinutes = minutes;
+    var countMinutes =25;
     var countSeconds = 0;
+    
+    var isWorktime = true;
+    var pause = false;
+    
+    var countDown;
+    
+    
+    updateStartingScreen();
+    
+    $("#start").click(function () {
+        if(pause==false){
+            countDown=setInterval(timer,1000);
+            pause = true;
+        }
+        else{
+            clearInterval(countDown);
+            pause = false;
+        }
+        
+    });
+    
+    // Increment or Decrement Work and Break Time Buttons
+    $("#container-buttons").on("click", "#work-time-decrement", function () {
+        minutes--;
+        updateStartingScreen();
+    });
+    $("#container-buttons").on("click", "#work-time-increment", function () {
+        minutes++;
+        updateStartingScreen();
+    });
+    $("#container-buttons").on("click", "#break-time-decrement", function () {
+        breakMinutes--;
+        updateStartingScreen();
+    });
+    $("#container-buttons").on("click", "#break-time-increment", function () {
+        breakMinutes++;
+        updateStartingScreen();
+    });
+
+
+function startTimer() {
+    if (isWorktime) {
+        countMinutes = minutes;
+        countSeconds = 0;
     }
-    else if(!isWorktime){
-    var countMinutes = breakMinutes;
-    var countSeconds = 0; 
+    else if (!isWorktime) {
+        countMinutes = breakMinutes;
+        countSeconds = 0;
     }
-    var countDown = function(){
-        if(countSeconds==0 && countMinutes==0){
+    
+   
+   
+    printStatus(isWorktime);
+    formatTimer(countMinutes, countSeconds);
+    var setIntervalID = setInterval(timer, 1000);
+};
+
+function timer(){
+   
+     if (countSeconds == 0 && countMinutes == 0) {
             clearInterval(setIntervalID);
             isWorktime = !isWorktime;
-            newTimer(minutes,breakMinutes,isWorktime);
-            
-        } else if(countSeconds==0 && countMinutes!=0){
+            startTimer(minutes, breakMinutes, isWorktime);
+        }
+        else if (countSeconds == 0 && countMinutes != 0) {
             countMinutes--;
             countSeconds = 59;
-            
-        } else{
+        }
+        else {
             countSeconds--;
         }
-        formatTimer(countMinutes,countSeconds);
-        //$("#timer").html(minutes+":"+seconds);     
-    };
-    clearInterval(setIntervalID);  
-    printStatus(isWorktime);
-    formatTimer(countMinutes,countSeconds);
-    var setIntervalID = setInterval(countDown,1000);
-    
+       formatTimer(countMinutes, countSeconds);
+}
 
-};
-
-function formatTimer(minutes,seconds){
-    if(minutes<10 && seconds<10){
-         $("#timer").html("0"+minutes+":"+"0"+seconds);
-    }else if(minutes<10){
-         $("#timer").html("0"+minutes+":"+seconds);
-    }else if(seconds<10){
-         $("#timer").html(minutes+":"+"0"+seconds);
-    }else{
-     $("#timer").html(minutes+":"+seconds);
+function formatTimer() {
+    if (countMinutes < 10 && countSeconds < 10) {
+        $("#timer").html("0" + countMinutes + ":" + "0" + countSeconds);
     }
-};
+    else if (countMinutes < 10) {
+        $("#timer").html("0" + countMinutes + ":" + countSeconds);
+    }
+    else if (countSeconds < 10) {
+        $("#timer").html(countMinutes + ":" + "0" + countSeconds);
+    }else{
+        $("#timer").html(countMinutes + ":" + countSeconds);
+    }
+    
+    
+}
 
-function printStatus(isWorkTime){
-    if(isWorkTime){
+function printStatus(isWorkTime) {
+    if (isWorkTime) {
         $("#timer-status").html("Get to Work!");
     }
-    else{
+    else {
         $("#timer-status").html("Take a Break");
     }
-};
+}
+
+function updateStartingScreen() {
+    countMinutes = minutes;
+    $("#work-time").html(minutes);
+    $("#break-time").html(breakMinutes);
+    $("#timer").html(minutes + ":" + "00");
+}
+});
