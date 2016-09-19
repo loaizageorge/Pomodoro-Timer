666ea46ffac3aaf1747ad466c4a1861dfc1cf869
George <loaizageorge@gmail.com>
2016-09-19 18:04:21 -0400

    Add reset button

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,35 @@
 $(document).ready(function () {
     
-  
+  // Default time used for resets and session transitions 
     
     var minutes = 25;
     var breakMinutes = 5;
     
-    var countMinutes =25;
+// The time that is actually decremented    
+    
+    var countMinutes = 25;
     var countSeconds = 0;
+    
+// App starts paused and in a work session
     
     var isWorktime = true;
     var pause = true;
+
     
-    var countDown;
+    var countDown; // Is used to store the setInterval function so it can be cleared from anywhere.
     var timesUp = document.getElementById("buzzer");
+     updateStartingScreen();
     
-      $('#work-time-slider').slider({
+// Handle Slider logic    
+    
+    $('#work-time-slider').slider({
          
 	formatter: function(value) {
         if(pause){
+        // Automatically update the numbers on both the clock(if currently on that session)    
         $("#work-time").html(value);
         $("#timer").html(value+":00");
+        minutes = value;    
         countMinutes=value;
         countSeconds=0;
 		return 'Current value: ' + value;
@@ -33,16 +43,27 @@ $(document).ready(function () {
         if(pause){
         $("#break-time").html(value);
         breakMinutes=value;
+        countMinutes = value;    
         countSeconds=0;
 		return 'Current value: ' + value;
         }
 	}
 });
     
+    // Handle Reset and Start/Stop buttons
     
-      
     
-    updateStartingScreen();
+    $("#reset-btn").click(function(){
+        
+        clearInterval(countDown);
+        pause=true;
+        countMinutes = minutes;
+        countSeconds = 0;
+         $("#start-stop-btn").toggleClass("fa-play-circle fa-pause-circle");
+        formatTimer();
+    }); 
+    
+   
     
     $("#start-stop-btn").click(function () {
         $("#start-stop-btn").toggleClass("fa-play-circle fa-pause-circle");
